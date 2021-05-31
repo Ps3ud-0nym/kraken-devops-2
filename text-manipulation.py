1e8cb85f39bcb918dc4e023166e8ef6b693ef626
Al Nolan <al.nolan@gmail.com>
2021-05-31 13:59:36 +0100

    Added comments

--- text-manipulation.py
+++ text-manipulation.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python
+# Solution taken from stackoverflow thread and modified to print entire hostname as opposed to just "docker":
+# https://stackoverflow.com/questions/51567427/find-specific-words-in-text-file-and-print-the-line-using-python/51567512 
+
 def my_function():
 
 
-    with open("/etc/hosts",'r') as f:
+    with open("/etc/hosts",'r') as f: # Removed unnecessary output file
       text = f.read()
       result_string = ''
 
       words = ["docker"]
-      text2 = text.split()
+      text2 = text.split() # - Changed from (".") to ()
       for itemIndex in range(len(text2)):
           for word in words:
               if word in text2[itemIndex]:
@@ -18,5 +21,5 @@ def my_function():
                       print(text2[itemIndex])
                       break
       print(result_string)
-
+# Removed unnecessary secondary result output
 my_function()
--- text-manipulation.sh
+++ text-manipulation.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
 # Obtain all hostnames related to docker from /etc/hosts
+# Print second column with awk, and then select only results containing
+# the string 'docker'
+
 awk '{ print $2 }' /etc/hosts | grep docker.internal
